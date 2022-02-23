1dc23b8885ddf3efed561d0db859a5cec4d920c3
aramh <aramh@LAPTOP-LDCDNTS1>
2022-02-23 16:19:01 +0400

    Add project files.

---
+++ ShareToGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShareToGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now we can share code! Yay...");
+        }
+    }
+}
